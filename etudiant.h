734b888bbee39c4087589e7b9ec9d4cd883525c6
ewidaltayou <ewidalt@gmail.com>
2025-12-29 17:13:49 +0100

    modification

--- etudiant.h
+++ etudiant.h
@@ -1,5 +1,6 @@
 #ifndef ETUDIANT_H
 #define ETUDIANT_H
+
 // structure de la date
 typedef struct{
     int jour;
@@ -9,29 +10,30 @@ typedef struct{
 // structure de l'etudiant
 typedef struct{
    char matricule[20];
-   char nom[20];
-   char prenom[20];
+   char nom[30];
+   char prenom[30] ;
    Date dateNaissance;
-   char departement[15];
+   char departement[30];
    char filiere[30];
-   char region[10];
+   char region[20];
    char sexe;
 }Etudiant;
-// gestion de la date
-int anneeCourante();
+//fonctions de gestion des dates
 Date creerDate();
+Date dateActuelle();
 int verifierDate(Date d);
 // operations de CRUD des etudiants
-Etudiant creerEtudiant(const char*nomFichier);
 int modifierEtudiant(const char*matricule,const char*nomFichier);
 int supprimerEtudiant(const char*matricule,const char*nomFichier);
-void afficherEtudint(Etudiant e);
+void afficherEtudiant(Etudiant e);
+Etudiant creerEtudiant(const char *nomFichier);
+void enregistrerEtudiant(const char *nomFichier, Etudiant student);
 // operations supplementaires
 int calculAge(const char*matricule,const char*nomFichier);
 int nombreEtudiant(const char*nomFichier);
 int verifierMatricule(const char*matricule);
 void afficherTousLesEtudiant(const char*nomFichier);
-void genererMatricule(const char matricule[], int annee, int numero);
+char *generate_matricule(const char *etablissement, int index);
 int prochainNumero();
 //pour les differentes recherches
 int rechercherEtudiantParMatricule(const char*matricule,const char*nomFichier);
@@ -42,7 +44,4 @@ void trierParNom(const char*nomFichier);
 void trierParMatricule(const char*nomFichier);
 void trierParRegion(const char*nomFichier);
 void trierParFiliere(const char*nomFichier);
-#endif
-
-
-
+#endif 
